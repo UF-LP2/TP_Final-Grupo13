659998a41838221a8bfac7dba7a2da3e30ec85ec
Tito Yanzon <anselmoyanzon@gmail.com>
2022-11-14 09:01:44 -0300

    Cambios

--- cPedido.cs
+++ cPedido.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.dll;
+//using System.Runtime.dll;
 using System.DirectoryServices;
 
 namespace tp_final
@@ -20,14 +20,14 @@ namespace tp_final
         protected eTipo tipo;
         protected int id_pedido;
         static int contador = 0;
-        protected var fecha;
+       // protected var fecha;
 
         public cPedido (List<cElectrodomestico> _listaE, eTipo _tipo, int _mes, int _dia)
         {
             this.listaE = _listaE;
             this.tipo = _tipo;
             this.id_pedido = contador;
-            this.fecha = new Datetime(2000, _mes, _dia, 0, 0, 0);
+            //this.fecha = new Datetime(2000, _mes, _dia, 0, 0, 0);
             contador++;
         }
         ~cPedido() {
