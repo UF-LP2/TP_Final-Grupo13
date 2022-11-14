0cf1849b94ff2dfb81fafdfae82b9f8ee75ae723
Tito Yanzon <anselmoyanzon@gmail.com>
2022-11-13 23:47:28 -0300

    Datos

--- cPedido.cs
+++ cPedido.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.dll;
+using System.DirectoryServices;
 
 namespace tp_final
 {
@@ -19,13 +20,14 @@ namespace tp_final
         protected eTipo tipo;
         protected int id_pedido;
         static int contador = 0;
-        protected var fecha = new Datetime(2000, 4, 5, 0, 0, 0);
+        protected var fecha;
 
-        public cPedido (List<cElectrodomestico> _listaE, eTipo _tipo)
+        public cPedido (List<cElectrodomestico> _listaE, eTipo _tipo, int _mes, int _dia)
         {
             this.listaE = _listaE;
             this.tipo = _tipo;
             this.id_pedido = contador;
+            this.fecha = new Datetime(2000, _mes, _dia, 0, 0, 0);
             contador++;
         }
         ~cPedido() {
--- cVehiculo.cs
+++ cVehiculo.cs
@@ -9,16 +9,19 @@ namespace tp_final
     internal class cVehiculo
     {
         protected int peso, patente;
-        protected float volumen, consumo;
-        protected bool aparato;
+        protected float volumen, nafta, consumo;
+        protected bool aparato, ahorro;
         static int contador = 123;
         protected List<cElectrodomestico> listaP = new List<cElectrodomestico>();
 
-        public cVehiculo(int _peso, float _volumen, float _consumo)
+        public cVehiculo(int _peso, float _volumen, float _nafta, List<cElectrodomestico> _lista, bool _ahorro, float _consumo)
         {
             this.peso = _peso;
             this.volumen = _volumen;
+            this.nafta = _nafta;
             this.consumo = _consumo;
+            this.listaP = _lista;
+            this.ahorro = _ahorro;
             this.aparato = false;
             this.patente = contador;
             contador++;
